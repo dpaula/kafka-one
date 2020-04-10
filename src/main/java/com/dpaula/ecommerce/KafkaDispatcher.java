2664fb3780790709bd0f8c906cb09364da59828f
Fernando de Lima <fernando.dpaula@gmail.com>
2020-04-09 23:14:28 -0300

    Padronizando o produtor de mensagens para um dispatcher padrao

--- src/main/java/com/dpaula/ecommerce/KafkaDispatcher.java
+++ src/main/java/com/dpaula/ecommerce/KafkaDispatcher.java
@@ -6,33 +6,35 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.Map;
 import java.util.Properties;
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-public class NewOrderMain {
+/**
+ * @author Fernando de Lima
+ */
+class KafkaDispatcher {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    private final KafkaProducer<String, String> producer;
 
-        var dispatcher = new KafkaDispatcher();
+    KafkaDispatcher() {
 
-        //para produzir uma mensagem, com tipo da chave e tipo da mensagem
-        var producer = new KafkaProducer<String, String>(properties());
+        this.producer = new KafkaProducer<>(properties());
+    }
 
-        for(var i = 0; i<10; i++) {
-            var key = UUID.randomUUID().toString();
+    private static Callback getCallback() {
+        return (dadosSucesso, excpFalha) -> {
+            //callback para tratar o retonro sincrono
 
-            var value = key + ",67144,8934844";
-            dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
-
-            var email = "Obrigado pelo pedido! Estamos processando seu pedido!";
-            dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
-        }
+            if (excpFalha != null) {
+                excpFalha.printStackTrace();
+                return;
+            }
+            System.out.println("Sucesso enviando : " + dadosSucesso.topic() + ":::partition " + dadosSucesso.partition() + "/ offset " + dadosSucesso.offset() + "/ timestamp " + dadosSucesso.timestamp());
+        };
     }
 
     // criando as propriedades na mão, mas deve ser pelo arquivo de properties
-    private static Properties properties() {
+    private Properties properties() {
 
         var properties = new Properties();
 
@@ -44,5 +46,15 @@ public class NewOrderMain {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         return properties;
+    }
+
+    public void send(String topico, String key, String value) throws ExecutionException, InterruptedException {
+
+        //mensagem que tera a mesma informação, tanto pra chave quanto o valor
+        var record = new ProducerRecord<>(topico, key, value);
+
+        // enviando uma mensagem
+        // com o .get ele fica sincrono
+        producer.send(record, getCallback()).get();
     }
 }
